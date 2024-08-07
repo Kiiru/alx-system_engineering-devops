150b54f427a9f013fe5e74e70e7aab80acdfe11d
Kiiru <mwangi.ramson@gmail.com>
2024-08-07 11:13:56 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,53 @@
 import requests
 import sys
 
-""" Python script that, using this REST API, for a given employee ID,
-    returns information about his/her TODO list progress.
+"""
+Python script that, using this REST API
+(https://jsonplaceholder.typicode.com),
+for a given employee ID, returns information
+about his/her TODO list progress.
+
+The script accepts an integer as a parameter,
+which is the employee ID.
+It displays on the standard output the employee
+TODO list progress in this exact format:
+
+First line: Employee EMPLOYEE_NAME is done with
+tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+    EMPLOYEE_NAME: name of the employee
+    NUMBER_OF_DONE_TASKS: number of completed tasks
+    TOTAL_NUMBER_OF_TASKS: total number of tasks,
+    which is the sum of
+    completed and non-completed tasks
+Second and subsequent lines display the title of
+completed tasks:
+TASK_TITLE (with 1 tabulation and 1 space
+before the TASK_TITLE)
+
+Example:
+    python3 0-gather_data_from_an_API.py 2
+    Employee Ervin Howell is done with tasks(8/20):
+         distinctio vitae autem nihil ut molestias quo
+         voluptas quo tenetur perspiciatis explicabo natus
+         aliquam aut quasi
+         veritatis pariatur delectus
+         nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis
+         repellendus veritatis molestias dicta incidunt
+         excepturi deleniti adipisci voluptatem et neque optio illum ad
+         totam atque quo nesciunt
 """
 
 
 def get_employee_todo_progress(e_id):
-    """ Operation to get employee todos progres"""
+    """
+    Function to fetch and display employee TODO list progress.
+
+    Parameters:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        None
+    """
     try:
         # get the employee info
         employee_url = f'https://jsonplaceholder.typicode.com/todos/{e_id}'
